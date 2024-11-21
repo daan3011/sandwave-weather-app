be635040ad47ac39cdddb84e441060280f5df603
daan3011 <daandorchholz@gmail.com>
2024-11-21 19:20:55 +0100

    added basic responsiveness to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,18 +13,18 @@
     </style>
 </head>
 <body class="bg-[#0C121E] text-white">
-    <div class="flex gap-5 w-full h-full p-5 overflow-auto">
+    <div class="flex flex-col lg:flex-row gap-5 w-full h-full p-5 overflow-auto">
         <!-- Sidebar -->
-        <aside class="bg-[#212B3C] text-white rounded-xl p-5 max-w-[10%] flex-shrink-0">
+        <aside class="bg-[#212B3C] text-white rounded-xl p-5 lg:max-w-[15%] w-full lg:w-auto flex-shrink-0">
             <div class="mb-10 flex justify-center">
                 <span>LOGO</span>
             </div>
-            <ul class="flex flex-col gap-4 text-center">
-                <li class="font-bold flex flex-col gap-2">
+            <ul class="flex lg:flex-col flex-row gap-4 lg:text-center text-sm lg:text-base">
+                <li class="font-bold flex flex-col gap-2 items-center">
                     <span>ICON</span>
                     Weather
                 </li>
-                <li class="font-bold flex flex-col gap-2">
+                <li class="font-bold flex flex-col gap-2 items-center">
                     <span>ICON</span>
                     Maps
                 </li>
@@ -32,7 +32,7 @@
         </aside>
 
         <!-- Main Content -->
-        <div class="flex flex-col gap-5 w-full">
+        <div class="flex flex-col gap-5 w-full lg:w-[60%]">
             <!-- Search Form -->
             <form action="" class="flex gap-2">
                 <input class="bg-[#212B3C] w-full p-2 rounded-lg text-white placeholder-gray-500" type="text" placeholder="Search for a city...">
@@ -40,8 +40,8 @@
             </form>
 
             <!-- Weather Overview -->
-            <div class="pl-12 py-8">
-                <div class="flex flex-col gap-10">
+            <div class="px-6 py-8">
+                <div class="flex flex-col gap-6">
                     <div>
                         <h1 class="text-3xl">Madrid</h1>
                         <span class="text-gray-400 text-sm">Chance of rain: 20%</span>
@@ -53,39 +53,51 @@
             <!-- Today's Forecast -->
             <div class="bg-[#212B3C] p-4 rounded-xl">
                 <h3 class="font-medium text-gray-500 uppercase pl-3 py-3">Today's forecast</h3>
-                <div class="flex gap-6 overflow-x-auto px-6">
+                <div class="flex gap-4 overflow-x-auto px-3">
                     <!-- Repeatable Forecast Cards -->
-                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-6">
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
                         <span>6AM</span>
                         <span class="text-5xl">☀️</span>
                         <span>25°C</span>
                     </div>
 
-                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-6">
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
                         <span>6AM</span>
                         <span class="text-5xl">☀️</span>
                         <span>25°C</span>
                     </div>
 
-                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-6">
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
                         <span>6AM</span>
                         <span class="text-5xl">☀️</span>
                         <span>25°C</span>
                     </div>
 
-                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-6">
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
                         <span>6AM</span>
                         <span class="text-5xl">☀️</span>
                         <span>25°C</span>
                     </div>
 
-                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-6">
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
                         <span>6AM</span>
                         <span class="text-5xl">☀️</span>
                         <span>25°C</span>
                     </div>
 
-                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-6">
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
+                        <span>6AM</span>
+                        <span class="text-5xl">☀️</span>
+                        <span>25°C</span>
+                    </div>
+
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
+                        <span>6AM</span>
+                        <span class="text-5xl">☀️</span>
+                        <span>25°C</span>
+                    </div>
+
+                    <div class="flex flex-col items-center gap-2 border-r border-gray-700 p-4 min-w-[100px]">
                         <span>6AM</span>
                         <span class="text-5xl">☀️</span>
                         <span>25°C</span>
@@ -96,8 +108,8 @@
             <!-- Air Conditions -->
             <div class="bg-[#212B3C] p-4 rounded-xl flex-grow">
                 <h3 class="font-medium text-gray-500 uppercase pl-3 py-3">Air Conditions</h3>
-                <div class="flex flex-wrap py-6">
-                    <div class="flex gap-4 w-1/2 p-3">
+                <div class="flex flex-wrap gap-4">
+                    <div class="flex gap-4 w-full sm:w-[48%] p-3">
                         <span class="text-5xl">☀️</span>
                         <div class="flex flex-col gap-1">
                             <span class="text-sm text-gray-500">Real Feel</span>
@@ -105,7 +117,7 @@
                         </div>
                     </div>
 
-                    <div class="flex gap-4 w-1/2 p-3">
+                    <div class="flex gap-4 w-full sm:w-[48%] p-3">
                         <span class="text-5xl">☀️</span>
                         <div class="flex flex-col gap-1">
                             <span class="text-sm text-gray-500">Real Feel</span>
@@ -113,7 +125,7 @@
                         </div>
                     </div>
 
-                    <div class="flex gap-4 w-1/2 p-3">
+                    <div class="flex gap-4 w-full sm:w-[48%] p-3">
                         <span class="text-5xl">☀️</span>
                         <div class="flex flex-col gap-1">
                             <span class="text-sm text-gray-500">Real Feel</span>
@@ -121,24 +133,24 @@
                         </div>
                     </div>
 
-                    <div class="flex gap-4 w-1/2 p-3">
+                    <div class="flex gap-4 w-full sm:w-[48%] p-3">
                         <span class="text-5xl">☀️</span>
                         <div class="flex flex-col gap-1">
                             <span class="text-sm text-gray-500">Real Feel</span>
                             <span>30°C</span>
                         </div>
                     </div>
-                    <!-- Add more air condition items as needed -->
+
                 </div>
             </div>
         </div>
 
         <!-- 7-Day Forecast -->
-        <div class="bg-[#212B3C] w-full p-8 rounded-xl max-w-[40%] flex-shrink-0">
+        <div class="bg-[#212B3C] w-full lg:w-[25%] p-8 rounded-xl flex-shrink-0">
             <h3 class="font-medium text-gray-500 uppercase">7-Day Forecast</h3>
             <div class="flex flex-col">
                 <!-- Repeatable Forecast Rows -->
-                <div class="flex justify-between items-center py-6 border-b border-gray-700">
+                <div class="flex justify-between items-center py-4 border-b border-gray-700">
                     <div class="text-gray-500">
                         <h4>Today</h4>
                     </div>
@@ -151,8 +163,7 @@
                     </div>
                 </div>
 
-
-                <div class="flex justify-between items-center py-6 border-b border-gray-700">
+                <div class="flex justify-between items-center py-4 border-b border-gray-700">
                     <div class="text-gray-500">
                         <h4>Today</h4>
                     </div>
@@ -165,7 +176,7 @@
                     </div>
                 </div>
 
-                <div class="flex justify-between items-center py-6 border-b border-gray-700">
+                <div class="flex justify-between items-center py-4 border-b border-gray-700">
                     <div class="text-gray-500">
                         <h4>Today</h4>
                     </div>
@@ -178,7 +189,7 @@
                     </div>
                 </div>
 
-                <div class="flex justify-between items-center py-6 border-b border-gray-700">
+                <div class="flex justify-between items-center py-4 border-b border-gray-700">
                     <div class="text-gray-500">
                         <h4>Today</h4>
                     </div>
@@ -191,7 +202,7 @@
                     </div>
                 </div>
 
-                <div class="flex justify-between items-center py-6 border-b border-gray-700">
+                <div class="flex justify-between items-center py-4 border-b border-gray-700">
                     <div class="text-gray-500">
                         <h4>Today</h4>
                     </div>
@@ -204,7 +215,7 @@
                     </div>
                 </div>
 
-                <div class="flex justify-between items-center py-6 border-b border-gray-700">
+                <div class="flex justify-between items-center py-4 border-b border-gray-700">
                     <div class="text-gray-500">
                         <h4>Today</h4>
                     </div>
@@ -217,7 +228,7 @@
                     </div>
                 </div>
 
-                <div class="flex justify-between items-center py-6 border-b border-gray-700">
+                <div class="flex justify-between items-center py-4 border-b border-gray-700">
                     <div class="text-gray-500">
                         <h4>Today</h4>
                     </div>
@@ -229,7 +240,6 @@
                         <span>33/44</span>
                     </div>
                 </div>
-                <!-- Add more rows as needed -->
             </div>
         </div>
     </div>
