80ea398a1d790c862bb4feb1a4cedb67d4ced93c
daan3011 <daandorchholz@gmail.com>
2024-11-24 14:55:44 +0100

    ensure weather codes fall within range for correct icon display

--- database/factories/WeatherReadingFactory.php
+++ database/factories/WeatherReadingFactory.php
@@ -6,9 +6,6 @@ use Carbon\Carbon;
 use App\Models\WeatherMonitor;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WeatherReading>
- */
 class WeatherReadingFactory extends Factory
 {
     const DECIMALS = 1;
@@ -22,8 +19,6 @@ class WeatherReadingFactory extends Factory
     const WIND_DIRECTION_MAX = 360;
     const CHANCE_OF_RAIN_MIN = 0;
     const CHANCE_OF_RAIN_MAX = 100;
-    const WEATHER_CODE_MIN = 100;
-    const WEATHER_CODE_MAX = 800;
     const DAY_MINUTES = 1440;
 
     /**
@@ -33,27 +28,43 @@ class WeatherReadingFactory extends Factory
      */
     public function definition(): array
     {
-        $weatherMonitor = WeatherMonitor::inRandomOrder()->first();
+        $weatherMonitor = WeatherMonitor::query()->inRandomOrder()->value('id');
+        $city = WeatherMonitor::find($weatherMonitor)?->city ?? 'Unknown';
+
+        $weatherConfigs = config('weather_icons', []);
+        $selectedWeather = $this->faker->randomElement($weatherConfigs);
+
+        $weatherCode = $this->faker->randomElement($selectedWeather['codes'] ?? []);
+
+        $descriptions = [
+            ['min' => 200, 'max' => 232, 'description' => 'Thunderstorm'],
+            ['min' => 300, 'max' => 321, 'description' => 'Light rain'],
+            ['min' => 500, 'max' => 531, 'description' => 'Rain'],
+            ['min' => 600, 'max' => 622, 'description' => 'Snow'],
+            ['min' => 701, 'max' => 781, 'description' => 'Mist'],
+            ['min' => 800, 'max' => 800, 'description' => 'Clear sky'],
+            ['min' => 801, 'max' => 804, 'description' => 'Cloudy'],
+        ];
+
+        // Match weather code
+        $weatherDescription = 'Unknown';
+        foreach ($descriptions as $range) {
+            if ($weatherCode >= $range['min'] && $weatherCode <= $range['max']) {
+                $weatherDescription = $range['description'];
+                break;
+            }
+        }
 
         return [
-            'weather_monitor_id' => $weatherMonitor->id,
-            'city' => $weatherMonitor->city,
+            'weather_monitor_id' => $weatherMonitor,
+            'city' => $city,
             'temperature' => $this->faker->randomFloat(self::DECIMALS, self::TEMPERATURE_MIN, self::TEMPERATURE_MAX),
             'feels_like' => $this->faker->randomFloat(self::DECIMALS, self::FEELS_LIKE_MIN, self::FEELS_LIKE_MAX),
-            'weather_description' => $this->faker->randomElement([
-                'Clear sky',
-                'Partly cloudy',
-                'Overcast',
-                'Light rain',
-                'Heavy rain',
-                'Thunderstorm',
-                'Snow',
-                'Fog'
-            ]),
-            'wind_speed' => $this->faker->randomFloat(self::DECIMALS, 0, 20),
+            'weather_description' => $weatherDescription,
+            'wind_speed' => $this->faker->randomFloat(self::DECIMALS, self::WIND_SPEED_MIN, self::WIND_SPEED_MAX),
             'wind_direction' => $this->faker->numberBetween(self::WIND_DIRECTION_MIN, self::WIND_DIRECTION_MAX),
             'chance_of_rain' => $this->faker->numberBetween(self::CHANCE_OF_RAIN_MIN, self::CHANCE_OF_RAIN_MAX),
-            'weather_code' => $this->faker->numberBetween(self::WEATHER_CODE_MIN, self::WEATHER_CODE_MAX),
+            'weather_code' => $weatherCode,
             'recorded_at' => Carbon::now()->subMinutes($this->faker->numberBetween(1, self::DAY_MINUTES)),
         ];
     }
