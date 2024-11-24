ff0cbcb458e7b509be20c87c63fe76739aa9a012
daan3011 <daandorchholz@gmail.com>
2024-11-24 13:43:33 +0100

    added realistic mock data and seeders

--- database/factories/WeatherMonitorFactory.php
+++ database/factories/WeatherMonitorFactory.php
@@ -12,13 +12,24 @@ class WeatherMonitorFactory extends Factory
     const INTERVAL_MINUTES_MIN = 30;
     const INTERVAL_MINUTES_MAX = 1440; // 24 hours
 
+    protected array $cities;
+
+    public function __construct(...$args)
+    {
+        parent::__construct(...$args);
+
+        $this->cities = json_decode(file_get_contents(database_path('sample-data/cities.json')), true);
+    }
+
     public function definition()
     {
+        $cityData = $this->faker->randomElement($this->cities);
+
         return [
-            'city' => $this->faker->city(),
-            'country' => $this->faker->country(),
-            'latitude' => $this->faker->latitude(),
-            'longitude' => $this->faker->longitude(),
+            'city' => $cityData['city'],
+            'country' => $cityData['country'],
+            'latitude' => $cityData['latitude'],
+            'longitude' => $cityData['longitude'],
             'interval_minutes' => $this->faker->numberBetween(self::INTERVAL_MINUTES_MIN, self::INTERVAL_MINUTES_MAX),
             'next_run_at' => Carbon::now()->addMinutes($this->faker->numberBetween(self::INTERVAL_MINUTES_MIN, self::INTERVAL_MINUTES_MAX)),
         ];
